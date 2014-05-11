e09a7974d645ff8aedc1b77d558bac81d7a21562
necocen <necocen@gmail.com>
2014-05-11 21:31:14 +0900

    socket.io server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var tweetSchema = require('../models/tweet.js');
+var utils = require('../bin/utils.js');
+var config = require('config');
+var debug = require('debug')('socket.io');
+
+mongoose.connect('mongodb://' + config.mongodb.host + ':' + config.mongodb.port + '/' + config.mongodb.db);
+var db = mongoose.connection;
+
+var Tweet = mongoose.model('Tweet', tweetSchema);
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+
 });
+
+router.search = function(socket) {
+  debug("connection");
+  socket.on('message', function(data) {
+    debug('message');
+    var args = data.value.split(/[ 　]/).map(function(arg){
+	    arg = arg.replace('\\', '\\\\').replace('"', '\\"').replace('\'', '\\\'').replace('(', '\\(').replace(')', '\\)').replace(' ', '\\ ');
+	    return 'text:@' + arg;
+	}).join(' + ');
+    debug(args);
+    utils.promiseToGroonga('/select?table=Tweets&query=' + encodeURIComponent(args) + '&output_columns=_key&limit=30', 'GET')
+	.then(function(value) {
+		var arr = JSON.parse(value)[1][0];
+		arr.shift();
+		arr.shift();
+		var ids = [].concat.apply([], arr);
+		Tweet.find({id_str: {'$in': ids}}, function(err, docs){
+			socket.emit('message', docs);
+		    });
+	    });
+      });
+  socket.on('disconnect', function() {
+	  debug('disconnect');
+      });
+};
 
 module.exports = router;
