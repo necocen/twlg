f4a7c6e606a883a4792c2ae09a2b3f4d3a35221c
necocen <necocen@gmail.com>
2014-05-11 19:44:25 +0900

    add crawler script

---
+++ bin/crawl.js
@@ -0,0 +1,65 @@
+var Twitter = require('twitter');
+var mongoose = require('mongoose');
+require('mongoose-long')(mongoose);
+var db = mongoose.connection;
+var tweetSchema = require('../models/tweet.js');
+var Tweet = mongoose.model('Tweet', tweetSchema);
+var BigNumber = require('bignumber.js');
+var JSONbig = require('json-bigint');
+var utils = require('./utils.js');
+var config = require('config');
+
+var client = new Twitter(config.twitter);
+mongoose.connect('mongodb://' + config.mongodb.host + ':' + config.mongodb.port + '/' + config.mongodb.db);
+
+
+// 保存されている最新のIDを取得してクロール開始
+var promise = utils.promiseToGroonga('/select?table=Tweets&output_columns=_key&sortby=-_key&limit=1', 'GET').then(function(value) {
+	var arr = JSONbig.parse(value)[1][0];
+	arr.shift();
+	arr.shift();
+	if(arr.length === 0) {
+	    console.log('start crawling')
+	    crawl();
+	} else {
+	    console.log('start crawling from: ' + arr[0][0].toString());
+	    crawl(arr[0][0].toString());
+	}
+    });
+
+function crawl(sinceId, maxId) {
+    var params = {
+	'user_id': config.twitter.user_id,
+	'include_rts': 1,
+	'count': 200,
+	'exclude_replies': false
+    };
+    if(sinceId !== undefined) {
+	params.since_id = sinceId;
+    }
+    if(maxId !== undefined) {
+	params.max_id = maxId;
+    }
+    client.get('/statuses/user_timeline.json', params, function(data) {
+	    if(data.length > 0) {
+		var firstId = new BigNumber(data[0].id_str);
+		var lastId = new BigNumber(data[data.length - 1].id_str);
+		console.log('retrieve ' + data.length + ' posts: ' + firstId.toString() + ' - ' + lastId.toString());
+		promise = promise.then(function(){
+			return Tweet.create(data, function() {
+				console.log('mongodb: ' + firstId.toString() + ' - ' + lastId.toString());
+			    }).then(function() {
+				    var json = JSON.stringify(data.map(function(tweet){
+						var createdAt = new Date(tweet.created_at);
+						var text = utils.urlExpandedText(tweet);
+						return {_key: tweet.id_str, text: text, created_at: createdAt.getTime() / 1000.0};		
+					    }));
+				    return utils.promiseToGroonga('/load?table=Tweets', 'POST', [json]);
+				}).then(function() {console.log('groonga: ' + firstId.toString() + ' - ' + lastId.toString());});
+		    });
+		crawl(sinceId, lastId.plus(-1).toString());
+	    } else {
+		promise = promise.then(function() {console.log('done'); return 0;}).finally(process.exit);
+	    }
+	});
+}
---
+++ bin/utils.js
@@ -0,0 +1,32 @@
+var http = require('q-io/http');
+var yaml = require('js-yaml');
+var fs = require('fs');
+var config = require('config');
+
+
+module.exports = {
+    // groongaになんか送ります
+    promiseToGroonga: function (path, method, body) {
+	return http.request({hostname: config.groonga.host,
+			     port: config.groonga.port,
+			     path: '/' + config.groonga.db + path,
+			     method: method,
+			     body: body,
+			     headers: {'Content-Type': 'application/json'}})
+	.then(function(response){return response.body.read();})
+	.then(function(buffer) {return buffer.toString('utf-8');});
+    },
+    // Tweetオブジェクトの短縮URLを展開したtextを返します
+    urlExpandedText: function(tweet) {
+	var text = tweet.text;
+	var extendedLength = 0; // URL置換によって伸びた長さの保存
+	tweet.entities.urls.forEach(function(url) {
+		var urlFrom = url.indices[0] + extendedLength;
+		var urlTo = url.indices[1] + extendedLength;
+		var urlLength = url.indices[1] - url.indices[0];
+		extendedLength += (url.expanded_url.length - urlLength);
+		text = text.substr(0, urlFrom) + url.expanded_url + text.substr(urlTo);
+	    });
+	return text;
+    }
+};
