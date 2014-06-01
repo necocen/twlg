3b3947cb3ee8be084720c810d5e975e91039e3c4
necocen <necocen@gmail.com>
2014-06-01 19:50:00 +0900

    検索の期間とリミット・オフセットと時間順序を指定できるようになりました

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,8 +10,17 @@ angular.module('twlg', ['twlgServices', 'twlgFilters', 'ngSanitize'])
 			 $scope.count = 0;
 			 $scope.query = '';
 			 $scope.$watch('query', function() {
-				 socket.emit('search', {value: $scope.query});
+				 search($scope.query);
 			 });
 			 $scope.baseTweet = baseTweetFilter;
 			 $scope.expandUrls = expandUrlsFilter;
+
+			 var search = function(query, skip, limit, dateMin, dateMax, order) {
+				 skip = skip || 0;
+				 limit = limit || 30;
+				 dateMin = dateMin || new Date(0); // distantPast
+				 dateMax = dateMax || new Date();  // now
+				 order = order || false;
+				 socket.emit('search', {query: query, skip: skip, limit: limit, dateMin: dateMin, dateMax: dateMax, order: order});
+			 };
 		 }]);
--- routes/index.js
+++ routes/index.js
@@ -21,22 +21,49 @@ router.search = function(socket) {
   debug("connection");
   socket.on('search', function(data) {
     debug('search');
-    var args = data.value.split(/[ 　]/).map(function(arg){
+    var args = data.query.split(/[ 　]/).map(function(arg){
+			if(arg.length === 0) return null;
 	    arg = arg.replace('\\', '\\\\').replace('"', '\\"').replace('\'', '\\\'').replace('(', '\\(').replace(')', '\\)').replace(' ', '\\ ');
 	    return 'text:@"' + arg + '"';
-	}).join(' + ');
+		}).filter(function(arg){
+			return (arg !== null);
+		}).join(' + ');
     debug(args);
-    utils.promiseToGroonga('/select?table=Tweets&query=' + encodeURIComponent(args) + '&sortby=-created_at&output_columns=_key&limit=30', 'GET')
-	.then(function(value) {
-		var arr = JSON.parse(value)[1][0];
-		var hitCount = arr.shift()[0];
-		var columns = arr.shift();
-		var ids = [].concat.apply([], arr);
-		Tweet.find({id_str: {'$in': ids}}, null, {sort: {created_at: -1}}, function(err, docs){
-			socket.emit('search', {result: docs, count: hitCount});
-		    });
-	    });
-      });
+
+		var dateMin = new Date(data.dateMin);
+		var dateMax = new Date(data.dateMax);
+		
+		if(args.length > 0) { // なにかの単語が
+			var query = ['table=Tweets',
+									 'query=' + encodeURIComponent(args),
+									 'sortby=' + (data.order ? '+' : '-') + 'created_at',
+									 'output_columns=_key',
+									 'filter=' + encodeURIComponent('(created_at>=' + (dateMin.getTime() / 1000.0).toString() + ')&&(created_at<' + (dateMax.getTime() / 1000.0).toString() + ')'),
+									 'offset=' + (data.skip || 0).toString(),
+									 'limit=' + Math.min(100, data.limit || 30).toString()];
+			debug(query.join('&'));
+			utils.promiseToGroonga('/select?' + query.join('&'), 'GET')
+				.then(function(value) {
+					var arr = JSON.parse(value)[1][0];
+					var hitCount = arr.shift()[0];
+					var columns = arr.shift();
+					var ids = [].concat.apply([], arr);
+					Tweet.find({id_str: {'$in': ids}})
+						.sort({created_at: (data.order ? 1 : -1)})
+							.exec(function(err, docs){
+								socket.emit('search', {result: docs, count: hitCount});
+							});
+				});
+		} else {
+			Tweet.find({created_at: {$gte: dateMin, $lt: dateMax}})
+				.sort({created_at: (data.order ? 1 : -1)})
+					.skip(data.skip || 0)
+						.limit(Math.min(100, data.limit || 30))
+							.exec(function(err, docs){
+								socket.emit('search', {result: docs, count: null});
+							});
+		}
+	});
   socket.on('disconnect', function() {
 	  debug('disconnect');
       });
